08fc962689ccb8f230437459da6fb52f92a7b7fe
karman <karman.98kaur@gmail.com>
2024-12-27 23:32:42 +0530

    completed controller to service transformation

--- BookLibrary/src/main/java/com/example/BookLibrary/controllers/BookController.java
+++ BookLibrary/src/main/java/com/example/BookLibrary/controllers/BookController.java
@@ -14,73 +14,71 @@ import java.util.List;
 public class BookController {
     private List<Book> booksList = new ArrayList<>(); //arraylist for saving the book in place of database
 
-    public BookController(){ //default constructor
-        booksList.add(new Book(1L,"Angel","Stephen"));
-        booksList.add(new Book(2L,"sweet 16","Malika"));
-        booksList.add(new Book(3L,"Atomic habits","Japenese"));
-        booksList.add(new Book(4L,"How to think","Nargois"));
+    public BookController() { //default constructor
+        booksList.add(new Book(1L, "Angel", "Stephen"));
+        booksList.add(new Book(2L, "sweet 16", "Malika"));
+        booksList.add(new Book(3L, "Atomic habits", "Japenese"));
+        booksList.add(new Book(4L, "How to think", "Nargois"));
     }
+
     @Autowired
     private BookService Obj;
-   // @GetMapping      //request URL = http://localhost:8080/books
-//    public List<Book> getAllBook(){
-//        return booksList;
-//    }
+
     @GetMapping
-    public List<Book> getAllBook(){
+    public List<Book> getAllBook() {
         return Obj.getAllBook();
     }
+
     @GetMapping("/{id}") //http://localhost:8080/books/4
-    public Book getBookById(@PathVariable Long id){
+    public Book getBookById(@PathVariable Long id) {
         return Obj.getBookById(id);
     }
-//    @GetMapping("/{id}") //http://localhost:8080/books/4
-//    public Book getBookById(@PathVariable Long id){
-//        for (Book book : booksList){
-//            if (book.getId().equals(id)){
-//                return book;
-//            }
-//        }
-//        return null;
-//    }
-//    {
-//        "name":"think big",
-//            "authorName":"Nepolean"
+
+    //    @PostMapping
+//    public Book createBook(@RequestBody Book clientBook){   //clienBook-outside var
+//        Long sizeOfBookList = Long.valueOf(booksList.size());  //size of current booklist = 4
+//        clientBook.setId((Long)(sizeOfBookList+1));           //where increase size to 5 and setting it to clientbook
+//        booksList.add(clientBook);               //saving clientbook in databse ie booklist
+//        return clientBook;   //return the clienbook with id for user confirmation
 //    }
     @PostMapping
-    public Book createBook(@RequestBody Book clientBook){   //clienBook-outside var
-        Long sizeOfBookList = Long.valueOf(booksList.size());  //size of current booklist = 4
-        clientBook.setId((Long)(sizeOfBookList+1));           //where increase size to 5 and setting it to clientbook
-        booksList.add(clientBook);               //saving clientbook in databse ie booklist
-        return clientBook;   //return the clienbook with id for user confirmation
+    public Book createBook(@RequestBody Book clientBook) {
+        return Obj.createBook(clientBook);
     }
 
+    //    @DeleteMapping("/{id}")
+//    public void deleteBook(@PathVariable Long id){
+//        for (Book book : booksList) {
+//            if (book.getId().equals(id)) {
+//                booksList.remove(book);
+//                break;
+//            }
+//        }
+//    }
     @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable Long id){
-        for (Book book : booksList) {
-            if (book.getId().equals(id)) {
-                booksList.remove(book);
-                break;
-            }
-        }
+    public void deleteBook(@PathVariable Long id) {
+        Obj.deleteBook(id);
     }
 
+
+    //    @PutMapping("/{id}")
+//    public Book updateBook(@PathVariable Long id, @RequestBody Book clientBook){
+//        Book currentBook = null;
+//        for(Book book : booksList){
+//            if(book.getId().equals(id)){
+//                currentBook = book;
+//                break;
+//            }
+//        }
+//        if (currentBook != null){
+//            currentBook.setName(clientBook.getname());
+//            currentBook.setAuthorName(clientBook.getAuthorName());
+//            return currentBook;
+//        }
+//        return  null;
+//    }
     @PutMapping("/{id}")
-    public Book updateBook(@PathVariable Long id, @RequestBody Book clientBook){
-        Book currentBook = null;
-        for(Book book : booksList){
-            if(book.getId().equals(id)){
-                currentBook = book;
-                break;
-            }
-        }
-        if (currentBook != null){
-            currentBook.setName(clientBook.getname());
-            currentBook.setAuthorName(clientBook.getAuthorName());
-            return currentBook;
-        }
-        return  null;
+    public Book updateBook(@PathVariable Long id, @RequestBody Book clientBook) {
+        return Obj.updateBook(id, clientBook);
     }
-
-
 }
--- BookLibrary/src/main/java/com/example/BookLibrary/service/BookService.java
+++ BookLibrary/src/main/java/com/example/BookLibrary/service/BookService.java
@@ -4,31 +4,62 @@ import com.example.BookLibrary.model.Book;
 import com.example.BookLibrary.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
-    private BookRepository Obj;
+    private BookRepository ObjRepo;
     // Constructor-based Dependency Injection
 //    public BookService(BookRepository Obj){
 //        this.Obj = Obj;
 //    }
     public List<Book> getAllBook() {
-        return Obj.findAll();
+        return ObjRepo.findAll();
     }
 
     public Book getBookById(Long id) {
-        List<Book> booksList = Obj.findAll();
+        List<Book> booksList = ObjRepo.findAll();
         for (Book book : booksList) {
             if (book.getId().equals(id)) {
                 return book; // Return directly from the list
             }
         }
         return null; // Return null if not found
+    }
+    //public Book createBook(@RequestBody Book clientBook){   //clienBook-outside var
+//        Long sizeOfBookList = Long.valueOf(booksList.size());  //size of current booklist = 4
+//        clientBook.setId((Long)(sizeOfBookList+1));           //where increase size to 5 and setting it to clientbook
+//        booksList.add(clientBook);               //saving clientbook in databse ie booklist
+//        return clientBook;   //return the clienbook with id for user confirmation
+//    }
+    public Book createBook(Book clientBook){
+        return ObjRepo.save(clientBook);
+    }
+
+    public void deleteBook(Long id){
+        List<Book> booksList = ObjRepo.findAll();
+        for (Book book: booksList){
+            if (book.getId().equals(id)){
+                ObjRepo.delete(book);
+            }
+        }
+        System.out.println("The Book with given Id:"+ id+ "is not present");
+    }
+
+    public Book updateBook(Long id,Book clientBook){
+        Book currentBook = ObjRepo.getReferenceById(id);
+        if (currentBook != null){
+            currentBook.setName(clientBook.getname());
+            currentBook.setAuthorName(clientBook.getAuthorName());
+            return ObjRepo.save(currentBook);
+        }
+        System.out.println("The Book with given Id:"+ id+ "is not present");
+
+
+        return  null;
     }
 
 }
